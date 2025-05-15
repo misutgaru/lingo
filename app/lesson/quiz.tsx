a52bfe0dcec08a87d7c05671dc9713f06ec466fd
genie <w-w@kakao.com>
2025-05-15 16:31:12 +0900

    Challenge Actions

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import { useState, useTransition } from 'react';
+import { toast } from 'sonner';
+
+import { reduceHearts } from '@/actions/user-progress';
 import { challengeOptions, challenges } from '@/db/schema';
-import { useState } from 'react';
+import { upsertChallengeProgress } from '@/actions/challenge-progress';
+
 import { Header } from './header';
-import { QuestionBubble } from './question-bubble';
-import { Challenge } from './challenge';
 import { Footer } from './footer';
+import { Challenge } from './challenge';
+import { QuestionBubble } from './question-bubble';
 
 type Props = {
   initialPercentage: number;
@@ -25,6 +30,8 @@ export const Quiz = ({
   initialLessonChallenges,
   userSubscription,
 }: Props) => {
+  const [pending, startTransition] = useTransition();
+
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
   const [challenges] = useState(initialLessonChallenges);
@@ -38,10 +45,75 @@ export const Quiz = ({
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions || [];
 
+  const onNext = () => {
+    setActiveIndex(current => current + 1);
+  };
+
   const onSelect = (id: number) => {
     if (status !== 'none') return;
 
     setSelectedOption(id);
+  };
+
+  const onContinue = () => {
+    if (!selectedOption) return;
+
+    if (status === 'wrong') {
+      setStatus('none');
+      setSelectedOption(undefined);
+      return;
+    }
+
+    if (status === 'correct') {
+      onNext();
+      setStatus('none');
+      setSelectedOption(undefined);
+      return;
+    }
+
+    const correctOption = options.find(option => option.correct);
+
+    if (!correctOption) {
+      return;
+    }
+
+    if (correctOption.id === selectedOption) {
+      startTransition(() => {
+        upsertChallengeProgress(challenge.id)
+          .then(response => {
+            if (response?.error === 'hearts') {
+              console.error('Missing hearts');
+              return;
+            }
+
+            setStatus('correct');
+            setPercentage(prev => prev + 100 / challenges.length);
+
+            // This is a practice
+            if (initialPercentage === 100) {
+              setHearts(prev => Math.min(prev + 1, 5));
+            }
+          })
+          .catch(() => toast.error('Something went wrong. Pleasy try again.'));
+      });
+    } else {
+      startTransition(() => {
+        reduceHearts(challenge.id)
+          .then(response => {
+            if (response?.error === 'hearts') {
+              console.error('Missing hearts');
+              return;
+            }
+
+            setStatus('wrong');
+
+            if (!response?.error) {
+              setHearts(prev => Math.max(prev - 1, 0));
+            }
+          })
+          .catch(() => toast.error('Something went wrong. Pleasy try again.'));
+      });
+    }
   };
 
   const title = challenge.type === 'ASSIST' ? 'Select the correct meaning' : challenge.question;
@@ -66,14 +138,14 @@ export const Quiz = ({
                 onSelect={onSelect}
                 status={status}
                 selectedOption={selectedOption}
-                disabled={false}
+                disabled={pending}
                 type={challenge.type}
               />
             </div>
           </div>
         </div>
       </div>
-      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
+      <Footer disabled={pending || !selectedOption} status={status} onCheck={onContinue} />
     </>
   );
 };
