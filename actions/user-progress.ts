670dac2dd55dc5a4b636bdc4bd0795375b3868c3
genie <w-w@kakao.com>
2025-05-16 14:22:54 +0900

    Shop

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -6,6 +6,7 @@ import { revalidatePath } from 'next/cache';
 import { auth, currentUser } from '@clerk/nextjs/server';
 
 import db from '@/db/drizzle';
+import { POINTS_TO_REFILL } from '@/constants/points';
 import { challengeProgress, challenges, userProgress } from '@/db/schema';
 import { getCourseById, getUserProgress } from '@/db/queries';
 
@@ -109,4 +110,31 @@ export const reduceHearts = async (challengeId: number) => {
   revalidatePath('/quests');
   revalidatePath('/leaderboard');
   revalidatePath(`/lesson/${lessonId}`);
+};
+
+export const refillHearts = async () => {
+  console.log('POINTS_TO_REFILL', POINTS_TO_REFILL);
+  const currentUserProgress = await getUserProgress();
+
+  if (!currentUserProgress) {
+    throw new Error('User progress not found');
+  }
+
+  if (currentUserProgress.hearts === 5) {
+    throw new Error('Hearts are already full');
+  }
+
+  if (currentUserProgress.points < POINTS_TO_REFILL) {
+    throw new Error('Not enough points');
+  }
+
+  await db
+    .update(userProgress)
+    .set({ hearts: 5, points: currentUserProgress.points - POINTS_TO_REFILL })
+    .where(eq(userProgress.userId, currentUserProgress.userId));
+
+  revalidatePath('/shop');
+  revalidatePath('/learn');
+  revalidatePath('/quests');
+  revalidatePath('/leaderboard');
 };
